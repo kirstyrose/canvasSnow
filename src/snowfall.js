50332d7ddc1722dcf4a5324434c20057a0464e13
Samuel <s-bridge@live.co.uk>
2014-11-06 10:27:53 +0000

    [JS] - Tweaked for a more realistic effect

--- src/snowfall.js
+++ src/snowfall.js
@@ -1,36 +1,27 @@
 ;(function() {
 
-	// Lets be strict! - It will help flag up bad practices.
 	'use strict';
 
-	// Set a version for future tracking/releases.
 	var version = '0.1';
 
 	var snowFall = function() {
 
 		var self = this;
 		
-		// Global variables for this app.
 		var canvas, context, limit = 50;
 
-		// An initialisation object, lets put all our 'set-up' functions in here.
 		var initialisation = {
 
-			// Initialisation handler, this is where it all starts.
 			handler: function() {
 
-				// Create the canvas tag and fire setCanvas as a callback.
 				initialisation.createCanvas(initialisation.setCanvas);
 
-				// Call the draw method in the canvasMethods object.
 				canvasMethods.handler();
 
 			},
 
-			// Canvas tag creation.
 			createCanvas: function(callback) {
 
-				// Set all the canvas stuff.
 				var canvas = document.createElement('canvas');
 					canvas.id = 'snowFall';
 					canvas.height = document.documentElement.clientHeight;
@@ -40,15 +31,12 @@
 					canvas.style.left = 0;
 					canvas.style.zIndex = -1;
 
-				// Append to the body
 				document.body.appendChild(canvas);
 
-				//Adding in a callback for this function, check to see if it's a function and call it if it is after all the above code has fired.
 				if(typeof callback == 'function') callback.call();
 
 			},
 
-			// Setting the global canvas and context variables so we can use them throughout.
 			setCanvas: function() {
 
 				canvas = document.getElementById('snowFall');
@@ -58,7 +46,6 @@
 
 		}
 
-		// Creating a canvasMethod object that will keep all our canvas drawing away from the rest of the app.
 		var canvasMethods = {
 
 			flakes: {},
@@ -75,7 +62,7 @@
 
 					var index = i,
 						x = Math.round(Math.random() * canvas.width),
-						radius = Math.round(Math.random() * 100);
+						radius = Math.round(Math.random() * 6);
 
 					canvasMethods.drawFlake(x, -10, radius, index);
 					
@@ -104,7 +91,11 @@
 					true
 				);
 
-				context.fillstyle = 'black';
+				var gradient = context.createRadialGradient(10, 0, 0, 850, 50, 1000);
+					gradient.addColorStop(0, 'rgba(255, 255, 255, 0.8)');
+					gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+
+				context.fillStyle = gradient;
 				context.fill();
 
 				canvasMethods.flakes[index] = {
@@ -117,17 +108,14 @@
 
 			},
 
-			// This makes the snowflaskes fall, by reading the object and getting the data above then redrawing!
 			flakeDrop: function() {
 
-				// For each entry in the flakes object, do it for each one...!
 				for(var entry in canvasMethods.flakes) {
 
 					var flake = canvasMethods.flakes[entry];
 
 					canvasMethods.drawFlake(
 
-						// Make the adjustments to the flakes positons here! - You can probably split this up into functions that get a number for you.
 						canvasMethods.calcXPos(flake),
 						canvasMethods.calcYPos(flake),
 						flake.radius,
@@ -162,13 +150,9 @@
 				} else {
 
 
-					return flake.x -5;
+					return flake.x +0.5;
 
 				}
-
-				
-
-
 
 			}, 
 
@@ -180,25 +164,20 @@
 
 				} else {
 
-					return flake.y +=(flake.radius / 5);
+					return flake.y +=(flake.radius / 8);
 					
 				}
 				
 			}
 
-			//+=(Math.random() * -1)
-
 		}
 
-		// When the object is created on window.load it runs everything inside, you need to make sure you fire the initialisation handler.
 		initialisation.handler();
 
 	}
 
-	// Wait for window load so it doesn't throw a hissy fit.
 	window.onload = function() {
 
-		// Make an global object called snowFall, accessible throughout the site via the console/script files.
 		window.snowFall = new snowFall();
 
 	};
